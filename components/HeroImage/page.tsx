e7de84173ecf305f3fc3f4c46a51c58a31112d3a
ihsan <ihksansanhas@gmail.com>
2023-08-10 20:28:17 +0800

    feat: add title to hero image

--- components/HeroImage/page.tsx
+++ components/HeroImage/page.tsx
@@ -2,9 +2,17 @@
 import Slider from "react-slick";
 import Image from "next/image";
 
-export default function HeroImage() {
+interface IHeros {
+  title?: string;
+  src?: any;
+}
+interface IHeroImage {
+  heros?: IHeros[];
+}
+
+export default function HeroImage({ heros }: IHeroImage) {
   return (
-    <section className="min-h-screen overflow-hidden">
+    <section className="md:min-h-screen overflow-hidden">
       <Slider
         dots={true}
         infinite={true}
@@ -13,33 +21,22 @@ export default function HeroImage() {
         slidesToScroll={1}
         className="w-full"
       >
-        <div>
-          <Image
-            className="w-full"
-            alt="hero"
-            src={"/images/fish-1.jpg"}
-            width={800}
-            height={800}
-          />
-        </div>
-        <div>
-          <Image
-            className="w-full"
-            alt="hero"
-            src={"/images/fish-1.jpg"}
-            width={800}
-            height={800}
-          />
-        </div>
-        <div>
-          <Image
-            className="w-full"
-            alt="hero"
-            src={"/images/fish-1.jpg"}
-            width={800}
-            height={800}
-          />
-        </div>
+        {heros?.map((hero, i) => (
+          <div key={i} className="relative">
+            <div className="max-w-screen-lg mx-auto">
+              <h1 className="absolute text-white text-7xl font-bold top-80 max-w-screen-md">
+                {hero.title}
+              </h1>
+            </div>
+            <Image
+              className="w-full h-full"
+              alt="hero"
+              src={`/images/${hero.src}`}
+              width={800}
+              height={800}
+            />
+          </div>
+        ))}
       </Slider>
     </section>
   );
--- containers/Home/partials/Hero.tsx
+++ containers/Home/partials/Hero.tsx
@@ -1,5 +1,20 @@
 import HeroImage from "@/components/HeroImage/page";
 
+const heros = [
+  {
+    title: "Connecting the Nation throught the fisheries supply cain",
+    src: "fish-1.jpg",
+  },
+  {
+    title: "Connecting the Nation throught the fisheries supply cain",
+    src: "fish-2.jpg",
+  },
+  {
+    title: "Connecting the Nation throught the fisheries supply cain",
+    src: "fish-3.jpg",
+  },
+];
+
 export default function Hero() {
-  return <HeroImage />;
+  return <HeroImage heros={heros} />;
 }
